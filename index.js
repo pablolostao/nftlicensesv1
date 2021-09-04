4c714ce958b9f1f6807876a81a63a39076aa82fd
Pablo <pablolostao@gmail.com>
2021-09-03 19:54:05 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/:tokenID', (req, res) => {
+  metadata = {
+    "name": "Windows 10 license",
+    "description": "This license is for Windows 10 Professional",
+    "image": "https://cdn.shopify.com/s/files/1/2158/1497/products/win10pro_x850.jpg?v=1571338137",
+    "attributes": [{
+      "trait_type": "Expiration", 
+      "value": "Never"
+    }, 
+    {
+      "trait_type": "Level", 
+      "value": "5"
+    }]
+  }
+  res.send(metadata)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
